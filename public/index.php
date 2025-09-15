add13ba1b79bfb37b28dab99868680e063e2af37
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-09-16 00:42:58 +0530

    Update index.php
    new chanes apppplited

--- public/index.php
+++ public/index.php
@@ -1,220 +1,25 @@
 <?php
 error_reporting(0);
 date_default_timezone_set('Asia/Kolkata');
-define('TOKEN_EXPIRY_TIME', 7000);
-define('KEY_FOLDER', '/var/www/secrets');
-function cUrlGetData($url, $headers = null, $post_fields = null)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 
-    if (!empty($post_fields)) {
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-    }
-
-    if (!empty($headers)) {
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    }
-
-    $data = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
-    }
-
-    curl_close($ch);
-    return $data;
-}
-
-// Encrypt data
-function encrypt_data($data, $key)
-{
-    $key = (int) $key;
-    $encrypted = array_map(function ($char) use ($key) {
-        return chr(ord($char) + $key);
-    }, str_split($data));
-    return base64_encode(implode('', $encrypted));
-}
-
-// Decrypt data
-function decrypt_data($e_data, $key)
-{
-    $key = (int) $key;
-    $encrypted = base64_decode($e_data);
-    $decrypted = array_map(function ($char) use ($key) {
-        return chr(ord($char) - $key);
-    }, str_split($encrypted));
-    return implode('', $decrypted);
-}
-
-function getCRED()
-{
-    $filePath = KEY_FOLDER.'/creds.jtv';
-    $key_data = file_get_contents(KEY_FOLDER.'/credskey.jtv');
-    return decrypt_data(file_get_contents($filePath), $key_data);
-}
-
-function getJioTvData($id)
-{
-    $cred = getCRED();
-    $jio_cred = json_decode($cred, true);
-    if (!$jio_cred) {
-        return null;
-    }
-
-    $user = isset($jio_cred['sessionAttributes']['user']) ? $jio_cred['sessionAttributes']['user'] : array();
-    $access_token = isset($jio_cred['authToken']) ? $jio_cred['authToken'] : '';
-    $crm = isset($user['subscriberId']) ? $user['subscriberId'] : '';
-    $uniqueId = isset($user['unique']) ? $user['unique'] : '';
-    $device_id = isset($jio_cred['deviceId']) ? $jio_cred['deviceId'] : '';
-
-    $post_data = http_build_query(array('stream_type' => 'Seek', 'channel_id' => $id));
-
-    $headers = array(
-        "Host: jiotvapi.media.jio.com",
-        "Content-Type: application/x-www-form-urlencoded",
-        "appkey: NzNiMDhlYzQyNjJm",
-        "channel_id: $id",
-        "userid: $crm",
-        "crmid: $crm",
-        "deviceId: $device_id",
-        "devicetype: phone",
-        "isott: true",
-        "languageId: 6",
-        "lbcookie: 1",
-        "os: android",
-        "dm: Xiaomi 22101316UP",
-        "osversion: 14",
-        "srno: 240303144000",
-        "accesstoken: $access_token",
-        "subscriberid: $crm",
-        "uniqueId: $uniqueId",
-        "content-length: " . strlen($post_data),
-        "usergroup: tvYR7NSNn7rymo3F",
-        "User-Agent: okhttp/4.9.3",
-        "versionCode: 353",
-    );
-
-    $response = cUrlGetData("https://jiotvapi.media.jio.com/playback/apis/v1/geturl", $headers, $post_data);
-    return json_decode($response, true);
-}
-function refresh_jio_token()
-{
-    $JIO_AUTH = json_decode(getCRED(), true);
-
-    if (!empty($JIO_AUTH)) {
-        $ref_TokenApi = "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken?langId=6";
-        $ref_TokenPost = '{"appName":"RJIL_JioTV","deviceId":"' . $JIO_AUTH['deviceId'] . '","refreshToken":"' . $JIO_AUTH['refreshToken'] . '"}';
-        $ref_TokenHeads = array(
-            "accesstoken: " . $JIO_AUTH['authToken'],
-            "uniqueId: " . $JIO_AUTH['sessionAttributes']['user']['unique'],
-            "devicetype: phone",
-            "versionCode: 331",
-            "os: android",
-            "Content-Type: application/json"
-        );
-
-        $process = curl_init($ref_TokenApi);
-        curl_setopt($process, CURLOPT_POST, 1);
-        curl_setopt($process, CURLOPT_POSTFIELDS, $ref_TokenPost);
-        curl_setopt($process, CURLOPT_HTTPHEADER, $ref_TokenHeads);
-        curl_setopt($process, CURLOPT_HEADER, 0);
-        curl_setopt($process, CURLOPT_TIMEOUT, 10);
-        curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
-        $ref_data = json_decode(curl_exec($process), true);
-        curl_close($process);
-
-        $resp = [
-            'status' => 'error',
-            'message' => '',
-            'authToken' => ''
-        ];
-
-        if (isset($ref_data['message']) && !empty($ref_data['message'])) {
-            $resp["message"] = "JioTV [OTP Login] - AuthToken Refresh Failed";
-        }
-
-        if (isset($ref_data['authToken']) && !empty($ref_data['authToken'])) {
-            $resp["status"] = "success";
-            $resp["message"] = "JioTV [OTP Login] - AuthToken Refreshed Successfully";
-            $resp["authToken"] = $ref_data['authToken'];
-        }
-    }
-
-    return $resp;
-}
-function getCookiesFromUrl($url, $headers = [], $post_fields = null)
-{
-    $ch = curl_init($url);
-    curl_setopt_array($ch, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HEADER => true,
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-        CURLOPT_AUTOREFERER => true,            
-    ]);
-
-    if ($post_fields !== null) {
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-    }
-
-    $response = curl_exec($ch);
-    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-    $header = substr($response, 0, $header_size);
-    curl_close($ch);
-
-    return extractCookies($header);
-}
-function extractCookies($header)
-{
-    $cookies = [];
-    foreach (explode("\r\n", $header) as $line) {
-        if (preg_match('/^Set-Cookie:\s*([^;]*)/mi', $line, $matches)) {
-            parse_str($matches[1], $cookie);
-            $cookies = array_merge($cookies, $cookie);
-        }
-    }
-    return $cookies;
-}
-function extractHdneaToken($raw)
-{
-    if (preg_match('/st=[^~]+~exp=[^~]+~acl=[^~]+~hmac=[a-f0-9]+/i', $raw, $matches)) {
-        // Force ACL to be /*
-        $token = preg_replace('/acl=[^~]+/', 'acl=/*', $matches[0]);
-        return '__hdnea__=' . $token;
-    }
-    return null;
-}
-$filePath = KEY_FOLDER.'/creds.jtv';
-$TokenNeedsRefresh = !file_exists($filePath) || (time() - filemtime($filePath) > TOKEN_EXPIRY_TIME);
-if ($TokenNeedsRefresh) {
-    $new_auth = refresh_jio_token();
-    $old_data = getCRED();
-    $old_data = json_decode($old_data, true);
-    $old_data["authToken"] = $new_auth["authToken"];
-    $new_auth = json_encode($old_data);
-    $key_data = file_get_contents(KEY_FOLDER."/credskey.jtv");
-    file_put_contents(KEY_FOLDER."/creds.jtv", encrypt_data($new_auth, $key_data));
-}
-$jsonData = getJioTvData(896);
-list($baseUrl, $query) = explode('?', $jsonData['result'], 2);
-$cooKiee = extractHdneaToken($query);
-
-//$jio_m3u_url = 'https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/jstar.m3u';
+$jio_m3u_url = 'https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/jstar.m3u';
 $zee5_m3u_url = 'https://raw.githubusercontent.com/alex8875/m3u/refs/heads/main/z5.m3u';
 $json_url = 'https://raw.githubusercontent.com/vijay-iptv/JSON/refs/heads/main/jiodata.json';
 
 // Load M3U and JSON
-//$jiom3u = file_get_contents($jio_m3u_url);
+$jiom3u = file_get_contents($jio_m3u_url);
 $zee5m3u = file_get_contents($zee5_m3u_url);
 $json = json_decode(file_get_contents($json_url), true);
-
+if (preg_match('/__hdnea__=[^"}]+/', $jiom3u, $matches)) {
+    $hdnea = $matches[0];
+} else {
+    // 2. Fallback: try to extract from URL query string
+    if (preg_match('/[?&]__hdnea__=([^&]+)/', $jiom3u, $matches)) {
+        $hdnea = '__hdnea__=' . $matches[1];
+    } else {
+        $hdnea = '';
+    }
+}
 $output = '#EXTM3U x-tvg-url="https://avkb.short.gy/jioepg.xml.gz"' . PHP_EOL;
 foreach ($json as $item) {
     if (isset($item['channel_id'], $item['logoUrl'], $item['channelLanguageId'])) {
@@ -229,8 +34,8 @@ foreach ($json as $item) {
         $output .= '#KODIPROP:inputstream.adaptive.license_type=clearkey' . PHP_EOL;
         $output .= '#KODIPROP:inputstream.adaptive.license_key=' . $item['license_key'] . PHP_EOL;
         $output .= '#EXTVLCOPT:http-user-agent=plaYtv/7.1.3 (Linux;Android 13) ygx/69.1 ExoPlayerLib/824.0' . PHP_EOL;
-        $output .= '#EXTHTTP:{"cookie":"'.$cooKiee.'"}'  . PHP_EOL;
-        $output .= 'https://jiotvmblive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$cooKiee.'&xxx=%7Ccookie='.$cooKiee . PHP_EOL . PHP_EOL;
+        $output .= '#EXTHTTP:{"cookie":"'.$hdnea.'"}'  . PHP_EOL;
+        $output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$hdnea.'&xxx=%7Ccookie='.$hdnea . PHP_EOL . PHP_EOL;
     }
 }
 // Process M3U lines
